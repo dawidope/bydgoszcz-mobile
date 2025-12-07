02722f70ee147da53934be08462860347c890e46
Dawid Wenderski <dawidope@gmail.com>
2025-12-07 01:24:47 +0100

    Force dark statusbar icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:bydgoszcz/di/injector.dart';
 import 'package:bydgoszcz/presentation/app/bydgoszcz_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Set system UI overlay style - dark icons on all screens
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.light,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
 
   await dotenv.load(fileName: ".env");
   await Injector().setup();
